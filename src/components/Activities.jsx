793c280e43b50107bbc48b2bbd9487c706111674
Abiola Oyewale <abiol4001@gmail.com>
2023-09-22 16:39:47 +0100

    fixed all bugs

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,30 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Activities = () => {
+  const activities = [
+    { name: "Meditation", icon: "moon.svg" },
+    { name: "Therapy", icon: "chart.svg" },
+    { name: "Natural Beauty", icon: "heart-circle.svg" },
+    { name: "Massages", icon: "emoji-happy.svg" },
+  ];
 
-    const activities = [
-      { name: "Meditation", icon: "moon.svg" },
-      { name: "Therapy", icon: "chart.svg" },
-      { name: "Natural Beauty", icon: "heart-circle.svg" },
-      { name: "Massages", icon: "emoji-happy.svg" },
-    ]
   return (
-    <div className="grid grid-cols-1 gap-y-8 gap-x-3 md:grid-cols-2">
-      {activities.map((activity) => (
-        <div className="flex flex-col gap-y-2">
-          <img src={activity.icon} alt="" className="h-[43px] w-[43px] object-cover" />
+    <section className="grid grid-cols-1 gap-y-8 gap-x-3 md:grid-cols-2 md:px-[100px] lg:px-0">
+      {activities.map((activity, index) => (
+        <article key={index} className="flex flex-col gap-y-2">
+          <img
+            src={activity.icon}
+            alt={`${activity.name} Icon`}
+            className="h-[43px] w-[43px] object-cover"
+          />
           <h3 className="font-bold text-2xl">{activity.name}</h3>
-          <p className='text-sm'>
+          <p className="text-sm">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
             varius enim in eros elementum tristique. Duis cursus, mi quis
             viverra ornare, eros dolor interdum nulla, ut commodo diam libero
             vitae erat.
           </p>
-          <button className="w-fit">Learn more</button>
-        </div>
+          <a
+            href="#" // Replace with actual links
+            className="w-fit bg-black text-white py-2 px-4 rounded-md hover:bg-white hover:ring-1 hover:ring-black hover:text-black transition duration-300 ease-in-out"
+          >
+            Learn more
+          </a>
+        </article>
       ))}
-    </div>
+    </section>
   );
-}
+};
 
-export default Activities
+export default Activities;
--- src/components/Treatments.jsx
+++ src/components/Treatments.jsx
@@ -1,60 +1,69 @@
-import React from 'react'
+import React from "react";
 
 const Treatments = () => {
+  const treatments = [
+    {
+      title: "Laser Treatment Consultation",
+      image: "treatment1.png",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.",
+      price: 85,
+      time: "3",
+    },
+    {
+      title: "Cool Sculpting Consultation",
+      image: "treatment2.png",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.",
+      price: 80,
+      time: "3",
+    },
+    {
+      title: "IPL Upper Chest",
+      image: "treatment3.png",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.",
+      price: 75,
+      time: "2",
+    },
+    {
+      title: "Underarm Botox",
+      image: "treatment4.png",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.",
+      price: 62,
+      time: "1",
+    },
+    {
+      title: "Laser Treatment Consultation",
+      image: "treatment5.png",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.",
+      price: 70,
+      time: "2",
+    },
+    {
+      title: "Body Botox",
+      image: "treatment6.png",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.",
+      price: 50,
+      time: "1",
+    },
+  ];
 
-    const treatments = [
-      {
-        title: "Laser Treatment Consultation",
-        image: "treatment1.png",
-        content: "",
-        price: 85,
-        time: "3",
-      },
-      {
-        title: "Cool Sculpting Consultation",
-        image: "treatment2.png",
-        content: "",
-        price: 80,
-        time: "3",
-      },
-      {
-        title: "IPL Upper Chest",
-        image: "treatment3.png",
-        content: "",
-        price: 75,
-        time: "2",
-      },
-      {
-        title: "Underarm Botox",
-        image: "treatment4.png",
-        content: "",
-        price: 62,
-        time: "1",
-      },
-      {
-        title: "Laser Treatment Consultation",
-        image: "treatment5.png",
-        content: "",
-        price: 70,
-        time: "2",
-      },
-      {
-        title: "Body Botox",
-        image: "treatment6.png",
-        content: "",
-        price: 50,
-        time: "1",
-      },
-    ];
   return (
     <div className="py-20">
-      <div className="px-4 lg:px-20 flex justify-center items-center gap-x-10 gap-y-10 flex-wrap">
-        {treatments.map((treatment) => (
-          <div className="w-full lg:w-[400px] flex flex-col gap-y-3 gap-x-10 flex-shrink-0">
-            <div className='w-full overflow-hidden'>
+      <div className="px-4 md:px-20 lg:px-20 flex justify-center items-center gap-x-10 gap-y-10 flex-wrap">
+        {treatments.map((treatment, index) => (
+          <div
+            key={index}
+            className="w-full lg:w-[400px] flex flex-col gap-y-3 gap-x-10 flex-shrink-0"
+          >
+            <div className="w-full overflow-hidden">
               <img
                 src={treatment.image}
-                alt=""
+                alt={treatment.title}
                 className="h-[304px] w-full object-cover hover:scale-105 transition-all ease-in-out"
               />
             </div>
@@ -64,11 +73,8 @@ const Treatments = () => {
                 ${treatment.price} for {treatment.time} Hours
               </div>
             </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse varius enim in eros elementum tristique.
-            </p>
-            <button class="w-fit ring-1 ring-black rounded-md px-4 py-2 hover:scale-105 transition-all">
+            <p>{treatment.content}</p>
+            <button className="w-fit ring-1 ring-black rounded-md px-4 py-2 hover:scale-105 transition-all">
               Book Now
             </button>
           </div>
@@ -76,12 +82,12 @@ const Treatments = () => {
       </div>
 
       <div className="w-full mt-20 flex justify-center">
-        <button class="w-fit ring-1 ring-black rounded-md px-4 py-2 hover:scale-105 transition-all mx-auto">
-          View all
+        <button className="w-full md:w-[200px] ring-1 ring-black rounded-md px-4 py-2 hover:scale-105 transition-all mx-auto">
+          View All
         </button>
       </div>
     </div>
   );
-}
+};
 
-export default Treatments
+export default Treatments;
